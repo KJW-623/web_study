1513065932ad366114cc3cf700f02521758d6cfd
KJW-623 <kimjiwoo4056@gmail.com>
2025-07-21 11:10:25 +0900

    연습15 고객/객실 신규 등록 관련 구조 만들기

---
+++ src/main/java/com/app/controller/study/practice/practice15/Practice15RoomController.java
@@ -0,0 +1,31 @@
+package com.app.controller.study.practice.practice15;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+
+@Controller
+public class Practice15RoomController {
+
+	@Autowired
+	Practice15RoomService practice15RoomService;
+
+	@GetMapping("/practice15/registerRoom")
+	public String registerCustomer() {
+		return "practice/practice15/registerRoom";
+	}
+
+	@PostMapping("/practice15/registerRoom")
+	public String registerCustomerAction(@ModelAttribute Practice15Room practice15Room) {
+		// 입력한 데이터 요청받는
+		System.out.println("[Controller] registerRoom");
+
+		// 저장하는 과정(service)
+		int result = practice15RoomService.saveRoom(practice15Room);
+
+		return "practice/practice15/registerRoom";
+	}
+
+}
